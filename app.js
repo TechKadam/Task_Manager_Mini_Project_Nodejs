cfb5df37cd67464377b92f8b2849434b2653fe04
ashish_kadam <ashish.kadam@quantiphi.com>
2023-07-26 19:41:54 +0530

    Create a separate folder called controllers wherein defined all the methods for crud operations of task and added them to respective routes in the routes folder.

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.get("/hello", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 
 // app.get('/api/v1/tasks')          - get all the tasks
-// app.post('/api/v1/tasks')         - get all the tasks
-// app.get('/api/v1/tasks/:id')      - get all the tasks
-// app.patch('/api/v1/tasks/:id')    - get all the tasks
-// app.delete('/api/v1/tasks/:id')   - get all the tasks
+// app.post('/api/v1/tasks')         - create a task
+// app.get('/api/v1/tasks/:id')      - get a single task with given id
+// app.patch('/api/v1/tasks/:id')    - update a task with given id
+// app.delete('/api/v1/tasks/:id')   - delte a task with given id
 const port = 3000;
 app.listen(port, console.log(`Server is listening on port ${port}...`));
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const {
+  getAllTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+  getTask,
+} = require("../controllers/tasks");
 const router = express.Router();
-router.route("/").get((req, res) => {
-  res.send("all items");
-});
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 module.exports = router;
